d709646fadce2a300fdff1cd2a73c88a71c0f477
forsrobin <forsrobin@gmail.com>
2024-01-18 14:24:39 +0100

    Add OpenSSL dependency and CMake presets

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,12 +1,17 @@
 include(FetchContent)
 
 option(BUILD_POSTGRESS "Build postgress" OFF)
+option(BUILD_OPENSSL "Build postgress" ON)
 
 include(cmake/dependencies/slint.cmake)
 
 if (BUILD_POSTGRESS)
     include(cmake/dependencies/postgres.cmake)
     target_link_libraries(${TARGET_NAME} PUBLIC libpqxx::pqxx)
+endif()
+
+if (BUILD_OPENSSL)
+    include(cmake/dependencies/openssl.cmake)
 endif()
 
 
--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,10 +1,9 @@
 #include "application_engine/application_engine.h"
 #include <string>
 
-
 int main(int argc, char **argv)
 {
-
+  
   auto slintApp = AppWindow::create();
   ApplicationEngine::init(slintApp);
   slintApp->run();
