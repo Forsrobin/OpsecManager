aeaeb0ff06bd7f22f371407a56913a78ab56aaa7
Forsrobin <forsrobin@gmail.com>
2024-01-20 16:55:50 +0100

    Add .env files to .gitignore and update launch.json with envFile

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,14 +1,13 @@
 #include "application_engine/application_engine.h"
-#include <string>
 
-#include <openssl/rsa.h>
+#include <string>
+#include <iostream>
 
 int main(int argc, char **argv)
 {
-  
+
   auto slintApp = AppWindow::create();
   ApplicationEngine::init(slintApp);
   slintApp->run();
-
   return 0;
 }
