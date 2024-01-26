c7c212a5ffa79f5962ef8735b8b77b5813e968fd
Forsrobin <forsrobin@gmail.com>
2024-01-26 18:48:01 +0100

    Enable building tests with Google Test and fix dependencies

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -6,12 +6,12 @@ if (BUILD_CURL)
     include(cmake/dependencies/curl.cmake)
 endif()
 
-if (BUILD_LIBPQXX)
-    include(cmake/dependencies/libpqxx.cmake)
-endif()
+# if (BUILD_LIBPQXX)
+#     include(cmake/dependencies/libpqxx.cmake)
+# endif()
 
-if (BUILD_OPENSSL)
-    include(cmake/dependencies/openssl.cmake)
-endif()
+# if (BUILD_OPENSSL)
+#     include(cmake/dependencies/openssl.cmake)
+# endif()
 
 
